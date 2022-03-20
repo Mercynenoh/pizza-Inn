270e9f760da0fdfd80c8a16c72fdf8fda48f769c
Mercynenoh <mercychepngenoh74@gmail.com>
2022-03-20 18:09:20 +0300

    add  function

--- js/script.js
+++ js/script.js
@@ -13,8 +13,19 @@ $("#fourth").click(function(){
 });
 });
 
+// cart working
 if (document.readyState == "loading"){
     document.addEventListener("DOMContentLoaded", ready);
 }else{
     ready();
 };
+function ready() {
+    // remove items from cart
+    let reomveCartButtons= document.getElementsByClassName("cart-remove");
+    console.log(reomveCartButtons)
+            for(let i=0; i<reomveCartButtons.length; i++){
+                let button= reomveCartButtons[i];
+                button.addEventListener("click", removeCartItem)
+            }
+        }
+        
