f771bc5fa589d1cc5dbfe854fb03118a5c0a69d3
Mercynenoh <mercychepngenoh74@gmail.com>
2022-03-20 18:00:21 +0300

    add jquery effects

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#first").click(function(){
+        $(".crispy").toggle()
+});
+$("#second").click(function(){
+    $(".glutten").toggle()
+});
+$("#third").click(function(){
+    $(".stuffed").toggle()
+});
+$("#fourth").click(function(){
+    $(".top").toggle()
+});
+});
